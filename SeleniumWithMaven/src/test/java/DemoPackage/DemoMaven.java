04797f7ac9c28634c8b8e1296dcc766aedff7db0
sumanmitra19 <mitr.suman99@gmail.com>
2019-01-05 02:04:23 +0530

    First commit

---
+++ SeleniumWithMaven/src/test/java/DemoPackage/DemoMaven.java
@@ -0,0 +1,20 @@
+package DemoPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DemoMaven {
+
+	@Test
+	public void PageTest()
+	{
+		System.out.println("Welcome to the World of Jenkins.....");
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\Softy\\Cell\\Drivers\\Chrome\\chromedriver.exe");
+		WebDriver dr = new ChromeDriver();
+		dr.get("https://www.facebook.com");
+		System.out.println("Page Title is : " + dr.getTitle());
+		System.out.println("Test Case finally passed and I'm ---->>>>> SUPER HAPPY man.hello>>>>>...");
+	}
+}
